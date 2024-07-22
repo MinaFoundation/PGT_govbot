94667f69c7f3396f89e8193d9fa618b0d4a88398
Illya Gerasymchuk <illya@iluxonchik.me>
2024-07-22 21:47:36 +0100

    feat: update admin Dashboard for refactor

--- src/channels/admin/dashboard.ts
+++ src/channels/admin/dashboard.ts
@@ -1,17 +1,9 @@
 // src/channels/admin/dashboard.ts
 
-import { BaseDashboard } from '../../core/BaseDashboard';
-import { HomeScreen } from './screens/HomeScreen';
-import { SMEManagementScreen } from './screens/SMEManagementScreen';
-import { Dashboard, ScreenConstructor } from '../../core/BaseClasses';
+import { Dashboard } from '../../core/BaseClasses';
 
-export class AdminDashboard extends BaseDashboard implements Dashboard {
-  static readonly DASHBOARD_ID = 'admin';
-  protected readonly initialScreenClass = HomeScreen;
-
-  constructor(homeScreenType: ScreenConstructor) {
-    super(homeScreenType);
-    this.registerScreen(new SMEManagementScreen(this, this.DEFAULT_VIEW_PERMISSION));
-  }
+export class AdminDashboard extends Dashboard {
+  
+  public static readonly ID = 'admin';
 
 }
