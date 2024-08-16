7d3e44373576c54e57d11df162ba426e03cc94ce
Illya Gerasymchuk <illya@iluxonchik.me>
2024-08-16 17:07:31 +0100

    feat: introduce automated pagination compoennts via actions

--- src/components/FundingRoundPaginator.ts
+++ src/components/FundingRoundPaginator.ts
@@ -1,24 +1,16 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Embed, EmbedBuilder, MessageActionRowComponentBuilder, StringSelectMenuBuilder } from "discord.js";
 import { FundingRoundLogic } from "../channels/admin/screens/FundingRoundLogic";
-import { Action, TrackedInteraction } from "../core/BaseClasses";
-import { EndUserError } from "../Errors";
+import { TrackedInteraction } from "../core/BaseClasses";
 import { FundingRound } from "../models";
-import { PaginationComponent } from "./PaginationComponent";
-import { ArgumentOracle, CustomIDOracle } from "../CustomIDOracle";
-import { Screen } from "../core/BaseClasses";
-import logger from "../logging";
+import { ORMModelPaginator, PaginationComponent } from "./PaginationComponent";
 
 /**
  * Reusable pagniator component for the selection of Funding Rounds. Invoke .showFundingRoundSelect() with
  * the action & operation to which the fundingRoundId selection shold be passed in the customId.
  */
-export class FundingRoundPaginator extends PaginationComponent {
-    public static readonly ID = 'fundingRoundPagination';
-    public readonly action: Action;
-    public readonly operation: string;
-    public readonly args: string[];
-    public readonly title: string;
-    public readonly customId: string;
+
+export abstract class FundingRoundPaginator extends ORMModelPaginator<FundingRound> {
+    public readonly description: string = 'To continue, select a Funding Round from the list below';
+    public readonly placeholder: string = 'Select a Funding Round';
 
 
     public static BOOLEAN = {
@@ -31,109 +23,58 @@ export class FundingRoundPaginator extends PaginationComponent {
     /**
      * Override this method to customize the list of Funding Rounds which should be presented/paginated.
      */
-    protected async getFundingRounds(interaction: TrackedInteraction): Promise<FundingRound[]> {
+    public async getItems(interaction: TrackedInteraction): Promise<FundingRound[]> {
         return await FundingRoundLogic.getPresentAndFutureFundingRounds();
+    } 
+
+    protected async getOptions(interaction: TrackedInteraction, items: FundingRound[]): Promise<any> {
+        return items.map((fr: FundingRound) => ({
+            label: fr.name,
+            value: fr.id.toString(),
+            description: `Budget: ${fr.budget}, Status: ${fr.status}`
+        }));
     }
 
-    constructor(screen: Screen, dstAction: Action, dstOperation: string, args: string[], title: string) {
-        super(screen, FundingRoundPaginator.ID);
-        this.action = dstAction;
-        this.operation = dstOperation;
-        this.args = args;
-        this.title = title;
-        this.customId =  CustomIDOracle.addArgumentsToAction(this.action, this.operation, ...this.args);
-    }
+}
 
-    protected async getTotalPages(interaction: TrackedInteraction): Promise<number> {
-        const fundingRounds = await this.getFundingRounds(interaction);
-        return Math.ceil(fundingRounds.length / 25);
-    }
+export class EditFundingRoundPaginator extends FundingRoundPaginator {
+    public static readonly ID = 'editFRPag';
 
-    protected async getItemsForPage(interaction: TrackedInteraction, page: number): Promise<FundingRound[]> {
-        const fundingRounds = await this.getFundingRounds(interaction);
-        return fundingRounds.slice(page * 25, (page + 1) * 25);
-    }
+    public args: string[] = []
+    public title: string = "Select A Funding Round To Edit";
+}
 
+export class SetCommitteeFundingRoundPaginator extends FundingRoundPaginator {
+    public args: string[] = [];
+    public title: string = "Select A Funding Round To Set Committee For";
+    public static readonly ID = 'setComFRPag';
 
-    public async handlePagination(interaction: TrackedInteraction): Promise<void> {
-
-        const currentPage = this.getCurrentPage(interaction);
-        const totalPages = await this.getTotalPages(interaction);
-        const fundingRounds = await this.getItemsForPage(interaction, currentPage);
-
-
-        const selectMenu = new StringSelectMenuBuilder()
-            .setCustomId(this.customId)
-            .setPlaceholder(`Select a Funding Round`)
-            .addOptions(fundingRounds.map((fr: FundingRound) => ({
-                label: fr.name,
-                value: fr.id.toString(),
-                description: `Budget: ${fr.budget}, Status: ${fr.status}`
-            })));
-
-        const embed = new EmbedBuilder()
-            .setTitle(this.title)
-            .setDescription(`To continue, select a Funding Round from the list below`);
-
-        const components: ActionRowBuilder<MessageActionRowComponentBuilder>[] = [
-            new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(selectMenu)
-        ];
-
-        if (totalPages > 1) {
-            const paginationRow = this.getPaginationRow(interaction, currentPage, totalPages);
-            components.push(paginationRow);
-        }
-
-        const isReply: boolean = ArgumentOracle.isArgumentEquals(interaction, FundingRoundPaginator.BOOLEAN.ARGUMENTS.FORCE_REPLY, FundingRoundPaginator.BOOLEAN.TRUE);
-        const data = { embeds: [embed], components };
-        
-        if (isReply) {
-            await interaction.respond(data);
-        } else {
-            await interaction.update(data);
-        }
-    }
-
-
-    public allSubActions(): Action[] {
-        return [];
-    }
-
-    public getPaginationRow(interaction: TrackedInteraction, currentPage: number, totalPages: number, ...args: string[]): ActionRowBuilder<ButtonBuilder> {
-        const row = new ActionRowBuilder<ButtonBuilder>();
     
-        if (currentPage > 0) {
-            const thisActionCustomId = CustomIDOracle.addArgumentsToAction(this, PaginationComponent.PAGINATION_ARG, PaginationComponent.PAGE_ARG, (currentPage - 1).toString(), ...args);
-          row.addComponents(
-            new ButtonBuilder()
-              .setCustomId(thisActionCustomId)
-              .setLabel('Previous')
-              .setStyle(ButtonStyle.Secondary)
-          );
-        }
-    
-        if (currentPage < totalPages - 1) {
-            const thisActionCustomId = CustomIDOracle.addArgumentsToAction(this, PaginationComponent.PAGINATION_ARG, PaginationComponent.PAGE_ARG, (currentPage + 1).toString(), ...args);
-          row.addComponents(
-            new ButtonBuilder()
-              .setCustomId(thisActionCustomId)
-              .setLabel('Next')
-              .setStyle(ButtonStyle.Secondary)
-          );
-        }
-    
-        return row;
-      }
+}
 
-    getComponent(...args: any[]): StringSelectMenuBuilder {
-        return new StringSelectMenuBuilder()
-            .setCustomId(CustomIDOracle.addArgumentsToAction(this, 'paginate'))
-            .setPlaceholder('Select a Funding Round');
-    }
+export class RemoveCommiteeFundingRoundPaginator extends FundingRoundPaginator {
+    public static readonly ID = 'remComFRPag';
+
+    public args: string[] = [];
+    public title: string = "Select A Funding Round To Remove Committee From";
+}
+
+export class ApproveRejectFundingRoundPaginator extends FundingRoundPaginator {
+    public static readonly ID = 'appRejFRPag';
+
+    public args: string[] = [];
+    public title: string = "Select A Funding Round To Approve Or Reject";
+
+    
 }
 
 export class InVotingFundingRoundPaginator extends FundingRoundPaginator {
-    protected async getFundingRounds(interaction: TrackedInteraction): Promise<FundingRound[]> {
+    public static readonly ID = 'inVotFRPag';
+    public args: string[] = [ORMModelPaginator.BOOLEAN.ARGUMENTS.FORCE_REPLY, ORMModelPaginator.BOOLEAN.TRUE]
+    public title: string = "Select A Funding Round To Vote For";
+
+
+    public async getItems(interaction: TrackedInteraction): Promise<FundingRound[]> {
         return await FundingRoundLogic.getEligibleVotingRounds();
     }
 }
--- src/components/PaginationComponent.ts
+++ src/components/PaginationComponent.ts
@@ -1,8 +1,11 @@
 // src/components/PaginationComponent.ts
 
 import { Action, TrackedInteraction } from '../core/BaseClasses';
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
-import { CustomIDOracle } from '../CustomIDOracle';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageActionRowComponentBuilder, StringSelectMenuBuilder } from 'discord.js';
+import { ArgumentOracle, CustomIDOracle } from '../CustomIDOracle';
+import { Screen } from '../core/BaseClasses';
+import { EndUserError } from '../Errors';
+import logger from '../logging';
 
 export abstract class PaginationComponent extends Action {
   public static readonly PAGE_ARG = 'page';
@@ -56,5 +59,126 @@ export abstract class PaginationComponent extends Action {
     } else {
       await this.handleInvalidOperation(interaction, operationId);
     }
+  }
+}
+
+/**
+ * Use this in the code, in the operation handler that shouls show the paginated list of items. Simply await on the handlePagination()
+ * and let it do the rest.
+ */
+export abstract class ORMModelPaginator<ORMModel> extends PaginationComponent {
+
+  public static BOOLEAN = {
+    TRUE: 'yes',
+    ARGUMENTS: {
+      FORCE_REPLY: 'fRp'
+    }
+  };
+
+  public readonly REQUIRED_ARGUMENTS: string[] = [];
+
+  public readonly action: Action;
+  public readonly operation: string;
+
+  public abstract readonly args: string[];
+
+  public abstract readonly title: string;
+  public abstract readonly description: string;
+  public abstract readonly placeholder: string;
+
+  constructor(screen: Screen, dstAction: Action, dstOperation: string, id: string) {
+    super(screen, id);
+    this.action = dstAction;
+    this.operation = dstOperation;
+  }
+
+  protected parseRequiredArguments(interaction: TrackedInteraction): { [key: string]: string } {
+    let parsedArguments: { [key: string]: string } = {};
+    for (const argument of this.REQUIRED_ARGUMENTS) {
+      logger.debug(`Checking for ${argument} in ${this.REQUIRED_ARGUMENTS}`);
+      const value: string = ArgumentOracle.getNamedArgument(interaction, argument);
+      parsedArguments[argument] = value;
+
+    }
+    return parsedArguments;
+  }
+
+  protected getRequiredArgumentsAsList(interaction: TrackedInteraction) :string[] {
+    const requiredArgs: {[key: string]: string} = this.parseRequiredArguments(interaction);
+    const requiredArgsAsList: string[] = requiredArgs ? Object.entries(requiredArgs).flat() : [];
+    return requiredArgsAsList;
+  }
+
+  protected abstract getOptions(interaction: TrackedInteraction, items: ORMModel[]): Promise<any>;
+  abstract getItems(interaction: TrackedInteraction): Promise<ORMModel[]>;
+
+  protected async getTotalPages(interaction: TrackedInteraction): Promise<number> {
+    const items = await this.getItems(interaction);
+    return Math.ceil(items.length / 25);
+  }
+
+  protected async getItemsForPage(interaction: TrackedInteraction, page: number): Promise<ORMModel[]> {
+    const items = await this.getItems(interaction);
+    return items.slice(page * 25, (page + 1) * 25);
+  }
+
+  public async handlePagination(interaction: TrackedInteraction): Promise<void> {
+    const currentPage = this.getCurrentPage(interaction);
+    const totalPages = await this.getTotalPages(interaction);
+    const allItems = await this.getItemsForPage(interaction, currentPage);
+
+    const paginatorStr: string = `Page ${currentPage + 1} of ${totalPages}`;
+    const fullDescription: string = `${this.description}\n\n${paginatorStr}`;
+
+    const requiredArgsAsList = this.getRequiredArgumentsAsList(interaction);
+    const allArgs: string[] = [...this.args, ...requiredArgsAsList]
+    const customId: string = CustomIDOracle.addArgumentsToAction(this.action, this.operation, ...allArgs);
+
+    const selectMenuOptions = await this.getOptions(interaction, allItems);
+    const selectMenu = new StringSelectMenuBuilder()
+      .setCustomId(customId)
+      .setPlaceholder(this.placeholder)
+      .addOptions(selectMenuOptions);
+
+    const embed = new EmbedBuilder()
+      .setTitle(this.title)
+      .setDescription(fullDescription);
+
+    const components: ActionRowBuilder<MessageActionRowComponentBuilder>[] = [
+      new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(selectMenu)
+    ];
+
+
+
+    console.log(requiredArgsAsList);
+
+    if (totalPages > 1) {
+      const paginationRow = this.getPaginationRow(interaction, currentPage, totalPages, ...requiredArgsAsList);
+      components.push(paginationRow);
+    }
+
+    await this.replyOrUpdate(interaction, { embeds: [embed], components: components });
+  }
+
+  protected async replyOrUpdate(interaction: TrackedInteraction, data: any): Promise<void> {
+    const isReply: boolean = ArgumentOracle.isArgumentEquals(interaction, ORMModelPaginator.BOOLEAN.ARGUMENTS.FORCE_REPLY, ORMModelPaginator.BOOLEAN.TRUE);
+
+    if (isReply) {
+      await interaction.respond(data);
+    } else {
+      await interaction.update(data);
+    }
+  }
+
+
+
+  public allSubActions(): Action[] {
+    return [];
+  }
+
+
+  getComponent(...args: any[]): StringSelectMenuBuilder {
+    throw new EndUserError('A paginator does not have a component');
+
   }
 }
